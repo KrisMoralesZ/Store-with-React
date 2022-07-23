5fef724d27c55bbafc8f8ce2fbde22bbfe400fc8
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-07-23 10:29:43 -0700

    css sass eslint & husky

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-const App = () => {
-  <h1>Hello Wold</h1>
-};
+import React from 'react';
+import '../styles/app.css';
+const App = () => <h1>Hello Wold</h1>;
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
-const HtmlWebpackPlugin =  require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { loader } = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
-  resolve:{
-    extensions: ['.js', '.jsx']
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
@@ -17,29 +18,42 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader'
-          }
-        ]
-      }
-    ]
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      filename: './index.html'
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
     }),
   ],
   devServer: {
     static: path.join(__dirname, 'dist'),
     compress: true,
     port: 3010,
-    open: true
-  }
-}
+    open: true,
+  },
+};
