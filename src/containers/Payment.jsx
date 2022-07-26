cd8c125e87158424b3a4f4b1e6246c8e22846e8c
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-07-25 18:40:01 -0700

    patment and success layouts

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import '../styles/components/Payment.css';
 const Payment = () => {
     return(
-        <h1>Payment</h1>
+       <div className="Payment">
+        <div className="Payment-content">
+          <h3>Resumen de Pedido</h3>
+          <div className="Payment-button">
+            Boton e pago con Paypal
+          </div>
+        </div>
+       </div> 
     );
 }
 export default Payment;
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import '../styles/components/Success.css';
 const Success = () => {
     return(
-        <h1>Success</h1>
+      <div className="Success">
+        <div className="Succes-content">
+          <h2>Gracias Por tu Compra</h2>
+          <span>Tu Pedido llegara en </span>
+          <div className="Success-map">
+            Google Maps
+          </div>
+        </div>
+      </div>
     );
 }
 export default Success;
