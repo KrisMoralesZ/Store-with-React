c418f38d8ce507ff3b5b117fd67f1b5609c84830
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-07-25 18:03:20 -0700

    Checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -3,7 +3,22 @@ import '../styles/components/Checkout.css';
 
 const Checkout = () => {
     return(
-        <h1>Checkout</h1>
+        <div className="Checkout">
+          <div className="Checkout-content">
+            <h3>Lista de Pedidos</h3>
+            <div className="Checkout-item">
+              <div className="Checkout-element">
+                <h4>ITEM name</h4>
+                <span>$10</span>
+              </div>
+              <button type="button">Eliminar</button>
+            </div>
+          </div>
+          <div className="Checkout-sidebar">
+            <h3>Precio total:</h3>
+            <button type="button">Continuar</button>
+          </div>
+        </div>
     );
 }
 export default Checkout;
